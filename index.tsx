4c2f2a3a5faf3fc390fdf2b5c5a33fe5fddf18b7
bekalu77 <bekalu77@gmail.com>
2026-01-08 01:54:20 +0300

    Prepare for frontend deployment

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './src/index.css';
-import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
